4973b488c50839756ac6200f8492298fdb91f2d1
Aleksandr Bocharov <alexbocharov2@gmail.com>
2023-04-10 16:43:53 +0300

    Control system

--- src/UAVControlSystem.cpp
+++ src/UAVControlSystem.cpp
@@ -38,15 +38,48 @@ UAVControlSystem::UAVControlSystem(const ParamsControlSystem *paramsControlSyste
 /**
  * @brief расчёт угловых скоростей роторов
  * 
- * @param stateVector вектор состояния БЛА
+ * @param stateVector вектор состояния БЛА, приходит из модели динамики
  * @param targetPoints целевая точка(x, y, z, угол рыскания)
  * @param time 
  * @return VectorXd_t 4 скорости для каждого ротора для модели динамики
  */
 VectorXd_t	UAVControlSystem::calculateMotorVelocity(StateVector stateVector, TargetPoints_t targetPoints, double time)
 {
-	auto targetPoint = this->motionPlanner->getCurrentTargetPoint();
+	auto targetPoint = this->motionPlanner->getCurrentTargetPoint(stateVector);
+	double yawDes = targetPoint(3);//Целевое рыскание
 
+	thrustController.SetTargetValue(targetPoint.z());
+	double pDes = thrustController.SetCurrentValue(stateVector.Z);//Команда по тяге
+	//auto rotationMatrix = Math::rotationMatrix2d(stateVector.Yaw);//todo: Матрица поворота
+
+	Eigen::Matrix2d rotationMatrix;
+	pitchController.SetTargetValue(targetPoint.y());
+	double pitchDes_st = this->pitchController.SetCurrentValue(stateVector.Y);//тангаж
+	rollController.SetTargetValue(targetPoint.x());
+	double rollDes_st = this->rollController.SetCurrentValue(stateVector.X);
+	Eigen::Vector2d angles;
+	angles << pitchDes_st, rollDes_st;
+	//Домножим матрицу поворота на упр. возд-я по тангажу и крену, чтобы перепроектировать их в связанную систему с учетом рысканья
+	Eigen::Vector2d angles_sv = rotationMatrix * angles;
+	double pitchDes = angles_sv(0);
+	double rollDes = angles_sv(1);
+	this->wxController.SetTargetValue(pitchDes);
+	double wX = this->wxController.SetCurrentValue(stateVector.Pitch);
+	this->wyController.SetTargetValue(rollDes);
+	double wY = wyController.SetCurrentValue(stateVector.Roll);
+	this->wzController.SetTargetValue(yawDes);
+	double wZ = wzController.SetCurrentValue(stateVector.Yaw);
+
+	//Получим команды по угловым ускорениям
+	awxController.SetTargetValue(wX);
+	double awx = awxController.SetCurrentValue(stateVector.PitchRate);
+	awyController.SetTargetValue(wY);
+	double awy = awyController.SetCurrentValue(stateVector.RollRate);
+	awzController.SetTargetValue(wZ);
+	double awz = awzController.SetCurrentValue(stateVector.YawRate);
+
+	auto rotorsCmd = mixer(pDes, awx, awy, awz);//Смешиваем команды и получаем команды для роторов
+	return rotorsCmd;
 }
 
 /**
